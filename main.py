38cd1e9dd98cd3424c8f1b78f54b7eed383a0bb2
berlangita <berlangito@gmail.com>
2025-04-07 12:35:39 +0200

    patata

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 asci = 31
+
+
 while asci < 255:
     char = chr(asci)
     print(f"{asci} : {char}".ljust(10), end="")
     if (asci - 31) % 5 == 0:
         print()
     asci += 1
+
+
 print("tabla asci")
+
